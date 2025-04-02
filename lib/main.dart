fb597c3cbeb37123c93290357775db0aca7c897b
KewalramaniDev <kewalramani2005@gmail.com>
2025-04-02 09:36:50 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Open-Square Chat',
+      title: 'Open-Square',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: primaryColor,
